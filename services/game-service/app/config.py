6b557c660d629098612baeca03161b0c5542f4bd
Sultan Amiour <sultan.amiour@epita.fr>
2026-06-03 11:11:20 +0200

    feat(game-service): add seed functionality

--- services/game-service/app/config.py
+++ services/game-service/app/config.py
@@ -1,14 +1,17 @@
 from pydantic_settings import BaseSettings
 
-
+# The moment a Settings obj is init, Pydantic autom looks at one's system env var's (.env),
+# searches for variable (Pydantic is smart enough to match uppercase env var to the lowercase Python attribute).
+# If it finds DATABASE_URL, it ignores hardcoded string and uses env value; else, falls back to default: "sqlite:///./games.db".
 class Settings(BaseSettings):
+    env: str = "local"
+
     database_url: str = "sqlite:///./games.db"
     redis_url: str = "redis://localhost:6379"
 
     # Added in Module 6 — must match SECRET_KEY in auth-service
     # auth_secret_key: str = "dev-secret-change-in-production"
 
-    model_config = {"env_file": ".env"}
-
+    model_config = {"env_file": ".env", "extra": "ignore"}  # 'ignore' prevents err when encountering other unrelated env var's
 
 settings = Settings()
--- services/game-service/app/main.py
+++ services/game-service/app/main.py
@@ -1,5 +1,55 @@
+import os
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.routes import router
 
-app = FastAPI(title="game-service")
+from app.routes import router
+from app.config import settings
+from app.database import DATABASE_URL, Base, engine, SessionLocal
+from app.schemas import GameCreate
+from app.models import Game
+from app.service import add_game
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db_path = DATABASE_URL.replace("sqlite:///", "")
+    db_needs_seeding = not os.path.exists(db_path)
+    
+    Base.metadata.create_all(bind=engine)
+    
+    # Seed
+    if db_needs_seeding and settings.env == "local":
+        db = SessionLocal()
+        try:
+            if db.query(Game).first() is None:
+                print(f"First run detected: Seeding initial game into {db_path}...")
+                
+                bb_gold = GameCreate(
+                    title="Battle Bears Gold",
+                    genre="Multiplayer Shooter",
+                    platform="iOS, Android",
+                    release_year=2013,
+                    cover_url="https://via.placeholder.com/300x400?text=Battle+Bears+Gold"
+                )
+                
+                minecraft = GameCreate(
+                    title="Minecraft",
+                    genre="Sandbox / Survival",
+                    platform="Cross-platform",
+                    release_year=2011, # Official full release year
+                    cover_url="https://via.placeholder.com/300x400?text=Minecraft"
+                )
+                
+                add_game(db, bb_gold)
+                add_game(db, minecraft)
+                
+                print(f"{db.query(Game).count()} Records successfully seeded.")
+        finally:
+            db.close()
+    
+    # When `yield` is hit, API starts accepting requests
+    yield 
+
+
+app = FastAPI(title="game-service", lifespan=lifespan)
+# Allows for use of APIRouter to modularize codebase
 app.include_router(router)
